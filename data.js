2d8197cebb735f03f6fb690046dd3a889abd5564
RemCake <Remembermenow1305@gmail.com>
2024-08-20 12:37:03 +0600

    rebase

---
+++ data.js
@@ -0,0 +1,255 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "Вид с востока",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.10951713705414079,
+        "pitch": 0.0024889733180355478,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.045725950554189154,
+          "pitch": 0.3438273128719356,
+          "rotation": 0,
+          "target": "1----"
+        },
+        {
+          "yaw": -0.19337694690024065,
+          "pitch": 0.13451496345957992,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1----",
+      "name": "Цех - склад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.11948792571346267,
+        "pitch": 0.347650282375934,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.45927674733738577,
+          "pitch": 0.2811921310828094,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 0.8213377376617892,
+          "pitch": 0.39820149635560753,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.38380997684298634,
+          "pitch": 0.10214110787161879,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Над территорией",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.091213121380072,
+        "pitch": 0.9759490432711821,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5757552970958084,
+          "pitch": 0.23865360612916753,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.338729341002983,
+          "pitch": 0.039302893568606834,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -0.8513506916156945,
+          "pitch": 0.13933323878485204,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Заезд",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.9470275109453006,
+        "pitch": 1.0605851921689027,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6392403410152134,
+          "pitch": 0.23391525832018978,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": -3.0296543924293324,
+          "pitch": 0.2857697108936801,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 1.2866043959475704,
+          "pitch": 0.02922934978881031,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "Вид с запада",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08860902356278899,
+        "pitch": 0.43226357861558995,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12584708809010436,
+          "pitch": 0.46528169599184466,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -0.31277878890993094,
+          "pitch": 0.16888417060440197,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.15194234554744668,
+          "pitch": -0.04845258703992528,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
